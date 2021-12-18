ebc93b6471887ebe379299f409e00d62ba34a3ce
Dominic Kolbe <kolbedominic@gmail.com>
2021-12-18 02:42:52 +0000

    inital project

---
+++ FastClipboardMenu/FastClipboardMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastClipboardMenuApp.swift
+//  FastClipboardMenu
+//
+//  Created by Dominic Kolbe on 18/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct FastClipboardMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
